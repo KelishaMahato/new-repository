99f4bb2467d33304e8697f77bda4757c673bf500
KelishaMahato <136666313+KelishaMahato@users.noreply.github.com>
2023-07-14 14:09:50 +0800

    assignment for module4 java
    assignment for the module four javascript i

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var names = [
+    "Fatima", 
+    "Karlo", 
+    "John Henry", 
+    "Tommy Henderson", 
+    "Journi Vega", 
+    "Callen Clark",
+    "Josue Villanueva",
+    "Jovie Landry", 
+    "Jazlyn Preston",
+    "Robin Butler"
+]
+
+for(var i=0; i< names.length; i++){
+    if (names[i][0]==="j" || names[i][0]==="J"){
+        console.log("Goodbye " + names[i] + "\n");
+}else{
+        console.log("Hello " + names[i] + "\n");
+}
+}
+
